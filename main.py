e863423b7801692bf3080662032e8878eca74b9d
Ziebhar <pythonexercise214@gmail.com>
2024-03-02 22:16:38 +0800

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import PyPDF2
 pdf_book=open("What-is-Enlightenment.pdf","rb")
 pdf_reader=PyPDF2.PdfReader(pdf_book)
 pages=len(pdf_reader.pages)
-print(pages)
+print(f"{pages} pages")
 
 speaker=pyttsx3.init()
 for index in range(0,pages):
