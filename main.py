51631227706358ca461b536c0d94098b14d96e00
Ziebhar <pythonexercise214@gmail.com>
2024-03-02 21:16:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyttsx3
+import PyPDF2
+
+pdf_book=open("What-is-Enlightenment.pdf","rb")
+pdf_reader=PyPDF2.PdfReader(pdf_book)
+pages=len(pdf_reader.pages)
+print(pages)
+
+speaker=pyttsx3.init()
+for index in range(0,pages):
+    chosen_page=pdf_reader.pages[index]
+    pdf_text=chosen_page.extract_text()
+    speaker.say(pdf_text)
+    speaker.runAndWait()
